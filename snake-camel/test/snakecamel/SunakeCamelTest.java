ef930f39985fe23faad6bc02e2d89d16d76d3140
g13945sm <g13945sm@s30441.tsuda.ac.jp>
2015-04-28 15:15:25 +0900

    テストケース追加

---
+++ snake-camel/test/snakecamel/SunakeCamelTest.java
@@ -0,0 +1,14 @@
+package snakecamel;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SunakeCamelTest {
+
+	@Test
+	public void test() {
+		fail("‚Ü‚¾ŽÀ‘•‚³‚ê‚Ä‚¢‚Ü‚¹‚ñ");
+	}
+
+}
